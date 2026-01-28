8beafe57e887f0305721a5122136e1fe66f22c38
LSpatricio <lpatricio@exsoinf.com>
2026-01-28 16:34:54 -0600

    feat(function): agregar function para la tabla admin esquema femco

---
+++ FUNCTION_FEMCO_BDI/Table/Custom/ADMIN/Function_ADMIN_FEMCO.cs
@@ -0,0 +1,174 @@
+﻿using FUNCTION_FEMCO_BDI.DAO;
+using FUNCTION_FEMCO_BDI.Funcionalidades;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FUNCTION_FEMCO_BDI.Table.Custom.ADMIN_FEMCO
+{
+    public class Function_ADMIN_FEMCO_FEMCO
+    {
+        private readonly ILogger _logger;
+        private readonly DAO_SQL _dao;
+        private readonly ICMService _icmservice;
+        private const string NOMBRE_TABLA = "FEMCO.ADMIN";
+
+        public Function_ADMIN_FEMCO_FEMCO(ILoggerFactory loggerFactory, DAO_SQL dao, ICMService icmService)
+        {
+            _logger = loggerFactory.CreateLogger<Function_ADMIN_FEMCO_FEMCO>();
+            _dao = dao;
+            _icmservice = icmService;
+
+        }
+
+        #region BulkCreate como método.
+        public async Task<string> BulkCreate_ADMIN_FEMCO()
+        {
+            string modeloICM = Environment.GetEnvironmentVariable("ModelFemco");
+            string TablaICM = "Admin";
+           
+            List<string> columnas = new List<string>
+            {
+                "AdminID",
+                "Name",
+                "Email",
+                "Created",
+                "RoleID",
+                "Disabled",
+                "FailedLogOnAttempts",
+                "LastPasswordExpirePrompt",
+                "LastPasswordExpireEmail",
+                "ChangePasswordAtNextLogin"
+            };
+            string parametros = "";
+            string mensaje = "";
+
+            string columnasFormateadas = FuncionalidadICM.FormatearColumnas(columnas);
+            string orderBy = $@" ORDER BY  {columnasFormateadas}";
+
+
+            string countConsulta = FuncionalidadICM.ConsultaAjustada(TablaICM, parametros);
+
+            string consultaICM = FuncionalidadICM.ConsultaAjustada(TablaICM, parametros, columnasFormateadas);
+
+            DataTable dtCount = await _icmservice.ConsultaICMQuerytool(TablaICM, countConsulta, modeloICM, 0);
+
+            int count = int.Parse(dtCount.Rows[0][0].ToString());
+
+            if (count == 0)
+            {
+                return "Sin datos por insertar en la tabla " + NOMBRE_TABLA;
+            }
+
+            await _dao.TruncateTable(NOMBRE_TABLA);
+
+            for (int i = 0; i < count; i += 500000)
+            {
+                DataTable dtParte = await _icmservice.ConsultaICMQuerytool(TablaICM, $"{consultaICM} {orderBy}", modeloICM, i);
+                mensaje = await _dao.bulkInsert(dtParte, NOMBRE_TABLA);
+            }
+
+
+            return mensaje;
+
+        }
+        #endregion
+
+        #region BulkCreate como Azure Function HTTPTrigger.
+
+        [Function("BulkCreate_Trigger_ADMIN_FEMCO")]
+        public async Task<HttpResponseData> BulkCreate_Trigger_ADMIN_FEMCO([HttpTrigger(AuthorizationLevel.Function, "post", Route = "BulkCreate_Trigger_ADMIN_FEMCO")] HttpRequestData req)
+        {
+            _logger.LogInformation("Inicio de la función BulkCreate_Trigger_ADMIN_FEMCO.");
+            var response = req.CreateResponse();
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+
+            try
+            {
+                string mensaje = await BulkCreate_ADMIN_FEMCO();
+
+                var result = new
+                {
+                    message = mensaje,
+                    timestamp = DateTime.UtcNow
+                };
+
+                if (mensaje.Contains("Sin datos por insertar"))
+                {
+                    response.StatusCode = HttpStatusCode.Accepted; // 202 Accepted
+                }
+                else
+                {
+                    response.StatusCode = HttpStatusCode.OK; // 200 OK
+                }
+
+                await response.WriteStringAsync(JsonConvert.SerializeObject(result));
+                _logger.LogInformation(mensaje);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ocurrió un error al procesar la solicitud: {Message}", ex.Message);
+
+                await response.WriteAsJsonAsync(new
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    errorCode = "INTERNAL_ERROR",
+                    message = "Ocurrió un error interno. Inténtalo más tarde.",
+                });
+                response.StatusCode = HttpStatusCode.InternalServerError;
+
+            }
+            finally
+            {
+                _logger.LogInformation("Fin de la función BulkCreate_Trigger_ADMIN_FEMCO");
+
+            }
+
+            return response;
+
+        }
+        #endregion
+
+        #region BulkCreate como Azure Function Timer.
+
+        //Diario dos ejecuciones. A las 8:30 am y 3:30 pm
+
+        [Function("BulkCreate_Timer_ADMIN_FEMCO")]
+        public async Task BulkCreate_Timer_ADMIN_FEMCO([TimerTrigger("0 30 8,15 * * *")] TimerInfo myTimer)
+        {
+
+            _logger.LogInformation("Inicio de la función BulkCreate_Timer_ADMIN_FEMCO.");
+
+            try
+            {
+                string mensaje = await BulkCreate_ADMIN_FEMCO();
+                _logger.LogInformation(mensaje);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "Error al ejecutar la función BulkCreate_Timer_ADMIN_FEMCO: {Message}", ex.Message);
+
+
+            }
+
+
+        }
+        #endregion
+
+
+
+
+    }
+}
