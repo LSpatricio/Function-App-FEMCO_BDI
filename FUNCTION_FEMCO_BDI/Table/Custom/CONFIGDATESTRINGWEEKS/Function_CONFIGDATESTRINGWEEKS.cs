b5036c4a0e92ec592f6b92c81d7c347fa04dd106
LSpatricio <lpatricio@exsoinf.com>
2026-02-19 15:29:44 -0600

    feat/agregar icm endpoints femcovs (#25)
    * feat(function): agregar endpoint cfgdates femcovs
    
    * feat(function): agregar endpoint ConfigDateStringWeeks femcovs

---
+++ FUNCTION_FEMCO_BDI/Table/Custom/CONFIGDATESTRINGWEEKS/Function_CONFIGDATESTRINGWEEKS.cs
@@ -0,0 +1,174 @@
+﻿using FUNCTION_FEMCO_BDI.DAO;
+using FUNCTION_FEMCO_BDI.Funcionalidades;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FUNCTION_FEMCO_BDI.Table.Custom.CONFIGDATESTRINGWEEKS
+{
+    public class Function_CONFIGDATESTRINGWEEKS
+    {
+        private readonly ILogger _logger;
+        private readonly DAO_SQL _dao;
+        private readonly ICMService _icmservice;
+        private const string NOMBRE_TABLA = "FEMCOVS.CONFIGDATESTRINGWEEKS";
+
+        public Function_CONFIGDATESTRINGWEEKS(ILoggerFactory loggerFactory, DAO_SQL dao, ICMService icmService)
+        {
+            _logger = loggerFactory.CreateLogger<Function_CONFIGDATESTRINGWEEKS>();
+            _dao = dao;
+            _icmservice = icmService;
+
+        }
+
+        #region BulkCreate como método.
+        public async Task<string> BulkCreate_CONFIGDATESTRINGWEEKS()
+        {
+            string modeloICM = Environment.GetEnvironmentVariable("ModelFemcoVS");
+            string TablaICM = "ConfigDateStringWeeks";
+
+            List<string> columnas = new List<string>
+            {
+                "IDPeriod",
+                "Period",
+                "StartDate",
+                "EndDate",
+                "IDCalendar",
+                "CalculationDate",
+                "CalculationDateValue",
+                "DaysOfMonth"
+            };
+            string parametros = "";
+            string mensaje = "";
+
+            string columnasFormateadas = FuncionalidadICM.FormatearColumnas(columnas);
+            string orderBy = $@" ORDER BY  {columnasFormateadas}";
+
+
+            string countConsulta = FuncionalidadICM.ConsultaAjustada(TablaICM, parametros);
+
+            string consultaICM = FuncionalidadICM.ConsultaAjustada(TablaICM, parametros, columnasFormateadas);
+
+            DataTable dtCount = await _icmservice.ConsultaICMQuerytool(TablaICM, countConsulta, modeloICM, 0);
+
+            int count = int.Parse(dtCount.Rows[0][0].ToString());
+
+            if (count == 0)
+            {
+                return "Sin datos por insertar en la tabla " + NOMBRE_TABLA;
+            }
+
+            await _dao.TruncateTable(NOMBRE_TABLA);
+
+            for (int i = 0; i < count; i += 400000)
+            {
+                DataTable dtParte = await _icmservice.ConsultaICMQuerytool(TablaICM, $"{consultaICM} {orderBy}", modeloICM, i);
+                mensaje = await _dao.bulkInsert(dtParte, NOMBRE_TABLA);
+            }
+
+
+            return mensaje;
+
+        }
+        #endregion
+
+        #region BulkCreate como Azure Function HTTPTrigger.
+
+        [Function("BulkCreate_Trigger_CONFIGDATESTRINGWEEKS")]
+        public async Task<HttpResponseData> BulkCreate_Trigger_CONFIGDATESTRINGWEEKS([HttpTrigger(AuthorizationLevel.Function, "post", Route = "BulkCreate_Trigger_CONFIGDATESTRINGWEEKS")] HttpRequestData req)
+        {
+            _logger.LogInformation("Inicio de la función BulkCreate_Trigger_CONFIGDATESTRINGWEEKS.");
+            var response = req.CreateResponse();
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+
+            try
+            {
+                string mensaje = await BulkCreate_CONFIGDATESTRINGWEEKS();
+
+                var result = new
+                {
+                    message = mensaje,
+                    timestamp = DateTime.UtcNow
+                };
+
+                if (mensaje.Contains("Sin datos por insertar"))
+                {
+                    response.StatusCode = HttpStatusCode.Accepted; // 202 Accepted
+                }
+                else
+                {
+                    response.StatusCode = HttpStatusCode.OK; // 200 OK
+                }
+
+                await response.WriteStringAsync(JsonConvert.SerializeObject(result));
+                _logger.LogInformation(mensaje);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ocurrió un error al procesar la solicitud: {Message}", ex.Message);
+
+                await response.WriteAsJsonAsync(new
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    errorCode = "INTERNAL_ERROR",
+                    message = "Ocurrió un error interno. Inténtalo más tarde.",
+                });
+                response.StatusCode = HttpStatusCode.InternalServerError;
+
+            }
+            finally
+            {
+                _logger.LogInformation("Fin de la función BulkCreate_Trigger_CONFIGDATESTRINGWEEKS");
+
+            }
+
+            return response;
+
+        }
+        #endregion
+
+        #region BulkCreate como Azure Function Timer.
+
+        //Todos los dias 3:00 pm
+        [Function("BulkCreate_Timer_CONFIGDATESTRINGWEEKS")]
+        public async Task BulkCreate_Timer_CONFIGDATESTRINGWEEKS([TimerTrigger("0 0 15 * * *")] TimerInfo myTimer)
+        {
+
+            _logger.LogInformation("Inicio de la función BulkCreate_Timer_CONFIGDATESTRINGWEEKS.");
+
+            try
+            {
+                string mensaje = await BulkCreate_CONFIGDATESTRINGWEEKS();
+                _logger.LogInformation(mensaje);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al ejecutar la función BulkCreate_Timer_CONFIGDATESTRINGWEEKS: {Message}", ex.Message);
+            }
+            finally
+            {
+                _logger.LogInformation("Fin de la función BulkCreate_Timer_CONFIGDATESTRINGWEEKS.");
+            }
+        }
+
+        #endregion
+
+
+
+
+    }
+}
